26bb91275a0775f44fdfaf83261cff2a2abb0189
determined-constant-1339 <curtis.brodie1011@yahoo.com>
2020-06-18 22:36:33 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    puts array[0]
+  elsif array.size == 2
+    puts array.join(" and ")
+  else 
+    puts array[0..-2].join(", ") + ", and " + array[-1]
+  end
 end
+
