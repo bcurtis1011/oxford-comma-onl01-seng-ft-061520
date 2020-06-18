0cf88cd63030d333b517f65f221f0131c3ea0c00
determined-constant-1339 <curtis.brodie1011@yahoo.com>
2020-06-18 22:38:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,10 @@
 def oxford_comma(array)
   if array.size == 1
     return array[0]
+  elsif array.size == 2
+    puts array.join(" and ")
+  else 
+    puts array[0..-2].join(", ") + ", and " + array[-1]
   end
 end
 
