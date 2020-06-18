f7aa1928a2bfb1e20f9ceaae21be79fc9f24671d
determined-constant-1339 <curtis.brodie1011@yahoo.com>
2020-06-18 22:38:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,9 @@ def oxford_comma(array)
   if array.size == 1
     return array[0]
   elsif array.size == 2
-    puts array.join(" and ")
+    return array.join(" and ")
   else 
-    puts array[0..-2].join(", ") + ", and " + array[-1]
+    return array[0..-2].join(", ") + ", and " + array[-1]
   end
 end
 
